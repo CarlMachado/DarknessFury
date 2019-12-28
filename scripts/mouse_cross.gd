12f8e83444392037ef002fdde0a86fc066610626
Carlos Machado <carlosedumachado@edu.univali.br>
2019-12-28 16:42:47 -0300

    correção cross

--- scripts/mouse_cross.gd
+++ scripts/mouse_cross.gd
@@ -4,4 +4,4 @@ func _ready():
     Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 func _process(delta):
-    $sprite.position = get_global_mouse_position() / 2
+	$sprite.position = get_global_mouse_position() / 2
