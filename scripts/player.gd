0d357883eb1ac033d07af034e33c8fb93894b9f6
Herikc <herikcbrecher@outlook.com.br>
2019-12-30 12:03:26 -0300

    Ataque do player, ajuste no timer do player e enemy. Falta ajustar angulo do tiro do player em relação ao mouse

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,12 @@
 extends KinematicBody2D
 
+const PRE_SPEAR = preload("res://prefabs/spear_hand.tscn")
 const MAX_LIFE = 100
 const SPEED = 200
 
 var life = MAX_LIFE
 var has_spear = false
+var loaded = true
 
 func _ready():
 	pass
@@ -29,10 +31,8 @@ func _physics_process(delta):
 	if not Input.is_action_pressed("ui_down") and not Input.is_action_pressed("ui_up") and not Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
 		$anim_sprite.play("idle")
 	
-	if Input.is_action_just_pressed("ui_attack"):
-		print("matar")
-		get_tree().call_group("spear", "spear_false")
-		has_spear = false
+	if Input.is_action_just_pressed("ui_attack") and has_spear and loaded:
+		shoot_spear()
 	
 	if has_spear:
 		$spear_hand.show()
@@ -42,9 +42,43 @@ func _physics_process(delta):
 	look_at(get_global_mouse_position())
 	move_and_slide(Vector2(x_dir, y_dir) * SPEED)
 
+func shoot_spear():
+	get_tree().call_group("spear", "spear_false")
+	var position_mouse = global_position.distance_to(get_global_mouse_position())
+	var correct
+	
+	print(position_mouse)
+	if position_mouse > 1150:
+		correct = 0.0109
+	if position_mouse > 1000:
+		correct = 0.0110
+	if position_mouse > 900:
+		correct = 0.0125
+	elif position_mouse > 600:
+		correct = 0.020
+	elif position_mouse > 300:
+		correct = 0.05
+	else:
+		correct = 0.175
+		
+	var spear_attack = PRE_SPEAR.instance()
+	spear_attack.global_position = $spear_hand.global_position / 2
+	spear_attack.rotation = global_rotation
+	spear_attack.dir = Vector2(cos(rotation + correct), sin(rotation + correct))
+	spear_attack.target_position = get_global_mouse_position()
+	get_parent().add_child(spear_attack)
+	
+	has_spear = false
+	loaded = false
+	$reload.start()
+
 func take_spear():
 	if has_spear:
 		get_tree().call_group("spear", "spear_true")
 	else:
 		get_tree().call_group("spear", "spear_true")
 		has_spear = true
+
+func _on_reload_timeout():
+	$reload.stop()
+	loaded = true
